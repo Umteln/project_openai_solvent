4acfdf828258ed7ce64e0c67ba34ba7cb3c5499b
Amera White <Amera.White@yahoo.com>
2023-01-09 20:54:13 -0600

    updated server url

--- client/script.js
+++ client/script.js
@@ -80,7 +80,7 @@ const handleSubmit = async (e) => {
 	// messageDiv.innerHTML = "..."
 	loader(messageDiv);
 
-	const response = await fetch('http://localhost:5001', {
+	const response = await fetch('https://solvent.onrender.com', {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
